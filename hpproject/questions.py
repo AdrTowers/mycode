a0d8e44ea56d747c16a4e003e2e9b3e42c581d73
Adrian Torres <adrian.torres@tlgcohort.com>
2021-08-04 17:11:43 +0000

    Added quiz game

---
+++ hpproject/questions.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3
+
+questions = {
+    0 : { "question": "What class interests you more?\nA) Defense against the Dark Arts\nB) Herbology\nC) History of Magic\nD) Potions",
+            "gryff": "A",
+            "huff": "B",
+            "raven": "C",
+            "slyth": "D"
+    },
+    1 : { "question": "What would you do if your friend wants to sneak out passed curfew?\nA) Prevent them from going\nB) Snitch on them\nC) Ignore them\nD) Tag along with them",
+            "gryff": "A",
+            "huff": "B",
+            "raven": "C",
+            "slyth": "D"
+    },
+    2 : { "question": "What's your favorite animal?\nA) Black Bear\nB) Cat\nC) Falcon\nD) Fox",
+            "gryff": "A",
+            "huff": "B",
+            "raven": "C",
+            "slyth": "D"
+    },
+    3 : { "question": "What position do you play on the Quidditch team?\nA) Seeker\nB) Keeper\nC) Chaser\nD) Beater",
+            "gryff": "A",
+            "huff": "B",
+            "raven": "C",
+            "slyth": "D"
+    },
+    4 : { "question": "What pet do you bring to Hogwarts?\nA) Owl\nB) Toad\nC) Cat\nD) Rat",
+            "gryff": "A",
+            "huff": "B",
+            "raven": "C",
+            "slyth": "D"
+    },
+    5 : { "question": "Your friends describe you as...\nA) Adventurous\nB) Humble\nC) Smart\nD) Cunning",
+            "gryff": "A",
+            "huff": "B",
+            "raven": "C",
+            "slyth": "D"
+    },
+    6 : { "question": "What would you want people to remember you by?\nA) Your ability to stand up for others\nB) The way you treat others\nC) The person that always knows what to do\nD) The person that always gets what they want",
+            "gryff": "A",
+            "huff": "B",
+            "raven": "C",
+            "slyth": "D"
+    },
+    7 : { "question": "What flavor of Bertie Botts Every Flavour Beans would you prefer?\nA) Cotton Candy\nB) Birthday Cake\nC) Butterbeer\nD) Chocolate",
+            "gryff": "A",
+            "huff": "B",
+            "raven": "C",
+            "slyth": "D"
+    },
+    8 : { "question": "What spell would you use the most?\nA) Expelliarmus\nB) Protego\nC) Alohomora\nD) Sectumsempra",
+            "gryff": "A",
+            "huff": "B",
+            "raven": "C",
+            "slyth": "D"
+    },
+    9 : { "question": "What is the material core of your wand?\nA) Phoenix feather\nB) Unicorn hair\nC) Hippogriff feather\nD) Dragon heartstring",
+            "gryff": "A",
+            "huff": "B",
+            "raven": "C",
+            "slyth": "D"
+    },
+    10 : { "question": "If you had a portrait that guarded your room, what would it be?\nA) Fat Lady\nB) Unicorn\nC) Knight\nD) Snake",
+            "gryff": "A",
+            "huff": "B",
+            "raven": "C",
+            "slyth": "D"
+    },
+    11 : { "question": "You look into the Mirror of Erised. What do you see?\nA) Yourself going on an adventure\nB) Yourself surrounded by life-long friends and family\nC) Yourself on a stage accepting awards\nD) Yourself being admired and praised by your peers",
+            "gryff": "A",
+            "huff": "B",
+            "raven": "C",
+            "slyth": "D"
+    }
+}
--- montypython/monty_python3.py
+++ montypython/monty_python3.py
@@ -5,7 +5,7 @@ answer = " "
 while round < 3 and answer != "Brian":
     round += 1     # increase the round counter by 1
     answer = input('Finish the movie title, "Monty Python\'s The Life of ______": ')
-    answer = answer.capitalize() # Capitalizes user input's first letter
+    answer = answer.title().strip() # Capitalizes user input's first letter
     if answer == "Brian": # logic to check if user gave correct answer
         print("Correct!")
     elif answer == "Shrubbery":
