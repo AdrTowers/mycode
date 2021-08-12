8a8c82398d316b21429ad69b4c58d393fa6fdb8d
Adrian Torres <adrian.torres@tlgcohort.com>
2021-08-12 04:40:03 +0000

    filtered through pokedex data and created new csv files

--- pokedex/pokedex.py
+++ pokedex/pokedex.py
@@ -1,20 +1,36 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+import pandas as pd
+import matplotlib.pyplot as plt
 
-import csv
+def main():
+    # import our file
+    csv_file = 'pokedex.txt'
 
-# open data file and loop through it
-with open("pokedex.txt", "r") as pokefile:
-    # counter to create unique file names
-    i = 0
-    # loop across our open file line by line
-    for row in csv.reader(pokefile):
-        i = i + 1  # increase i by 1 (to create unique admin.rc file names)
-        # this f string says "fill in the value of i"
-        filename = f"poke.txt{i}"
+    # store pokedex data
+    pokedex1 = pd.read_csv(csv_file, index_col=0)
+    # DF has 5 rows and 13 columns
+    print("First 20 pokemon\n", pokedex1.head(20))
 
-        # open a file via "with". This file will autoclose when the indentations stop
-        with open(filename, "w") as pokemonfile:
-            print("This is a test of my file" + row[0], file=pokemonfile)
+    # sort by the pokemon Type 1
+    sorted_by_Type1 = pokedex1.sort_values(["Type 1"], ascending=False)
 
-# Display when the looping is complete
-print("File has been created!")    
+    # Sort by the pokemon Name
+    sorted_by_name = pokedex1.sort_values(["Name"], ascending=True)
+
+    # Data is sorted by values in a column
+    print("Sorted by Type 1\n", sorted_by_Type1.head(20))
+    print("Sorted by Name\n", sorted_by_name.head(10))
+
+    # Created smaller list of columns for the last twenty rows
+    slimlist = pokedex1[["Name", "Type 1", "Type 2", "Generation"]]
+
+    print(slimlist.tail(20))
+
+    # Save to new files
+    sorted_by_Type1.head(20).to_csv("typehead.csv")
+    sorted_by_name.tail(20).to_csv("nametwenty.csv")
+    slimlist.tail(50).to_csv("slimpokedex.csv")
+
+
+if __name__ == "__main__":
+    main()
