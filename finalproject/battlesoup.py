254d2a7652d5339201e4e787667d9370f59a2702
Adrian Torres <adrian.torres@tlgcohort.com>
2021-08-13 09:25:11 +0000

    Currently able to print out some of the intended data

--- finalproject/battlesoup.py
+++ finalproject/battlesoup.py
@@ -4,7 +4,7 @@ import requests
 
 
 def main():
-    
+
     print(f"""
                 ╔══╗           ╔╗           ╔╗ ╔╗╔═══╗            
                 ║╔╗║           ║║           ║║ ║║║╔═╗║            
@@ -18,10 +18,10 @@ def main():
     """)
 
     # Ask the user for input in order to submit request
-    user_link = input("Enter the wikipedia url of the movie you wish to find out more about.\n")
+    #user_link = input("Enter the wikipedia url of the movie you wish to find out more about.\n")
 
     # Use requests to load content url
-    url = requests.get(user_link)
+    url = requests.get("https://en.wikipedia.org/wiki/Avengers:_Endgame")
     print(url)
 
     # implement BeautifulSoup to convert content into a bs object
@@ -33,22 +33,43 @@ def main():
 
     # Infosection stores all of the right hand side information found on the wiki page
     infosection = soup.find(class_="infobox vevent")
-    # print(infobox.prettify())
+    # print(infosection.prettify())
 
-    # Rows will store the data for each tr tag inside of our infobox
-    rows = infosection.find_all("tr")
-    for row in rows:
-        infobox = row.find("th", class_="infobox-label")
-        infobox_values = row.find("td", class_="infobox-data")
+    # Rows will store the data for each tr tag inside of our infosection
+    # rows = infosection.find_all("tr")
+    title = infosection.find("th", class_="infobox-above summary").get_text()
+    infobox = infosection.find("th", class_="infobox-label").get_text()
+    infobox_values = infosection.find("td", class_="infobox-data").get_text()
+    print(f"""
+    {title}
+    {infobox}: {infobox_values}
+    """)
 
-        # if infobox is not None:
-        #     # info_label = infobox.get_text()
-        #     # infobox_data = infobox_values.get_text()
+    # # # This method could have delivered the intended results but .get_text() will only work when used outside of
+    # # # the for loop
+    # for row in infosection:
+    #     title = row.find_all("th", class_="infobox-above summary")
+    #     infobox = row.find_all("th", class_="infobox-label")
+    #     infobox_values = row.find_all("td", class_="infobox-data")
+    #     print(f"""
+    #     {title}
+    #     {infobox}: {infobox_values}
+    #     """)
 
-        print(f"""
-        {infobox}: {infobox_values}
-        """)
+    # for row in rows:
+    #     infobox = row.find("th", class_="infobox-label")
+    #     infobox_values = row.find("td", class_="infobox-data")
+    #
+    #     # if infobox is not None:
+    #     #     # info_label = infobox.get_text()
+    #     #     # infobox_data = infobox_values.get_text()
+    #
+    #     print(f"""
+    #     {infobox}: {infobox_values}
+    #     """)
+
 
 
 if __name__ == '__main__':
     main()
+
